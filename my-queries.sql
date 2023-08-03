61bae19a71ad4d3d56c5c32dbc36b8b96afc317b
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-08-03 18:53:02 +0100

    completing more prompt queries

--- my-queries.sql
+++ my-queries.sql
@@ -1,6 +1,6 @@
 USE fcc;
 
--- Find all employess ordered by salary
+-- Find all employess ordered by salary (this is a "prompt" question)
 SELECT * FROM employee ORDER BY salary DESC;
 
 -- Find all employees ordered by sex then name
@@ -49,4 +49,66 @@ SELECT SUM(total_sales), client_id FROM works_with GROUP BY client_id;
 SELECT * FROM client WHERE client_name LIKE '%LLC%';
 
 -- Find any supplier with Label in their name
-SELECT * FROM branch_supplier WHERE client_name LIKE '%LLC%';
+SELECT * FROM branch_supplier WHERE supplier_name LIKE '%Label%';
+
+-- Find any employ born in October (date is in format 2000-01-01)
+SELECT * FROM employee WHERE birth_day LIKE '____-10-__';
+
+-- Find any clients who might be schools
+SELECT * FROM client WHERE client_name LIKE '%chool%';
+
+-- ----- SQL Unions (a "weak" join, combines two select statements)
+-- Find a list of employee and branch names
+SELECT first_name FROM employee;  -- just emplyee names
+SELECT branch_name FROM branch;   -- just name of branches
+SELECT first_name FROM employee UNION SELECT branch_name FROM branch;   -- Union
+SELECT first_name FROM employee UNION SELECT branch_name FROM branch UNION SELECT client_name FROM client;
+
+-- Find a list of all clients & branch suppliers' names
+SELECT client_name FROM client UNION SELECT supplier_name FROM branch_supplier; 
+
+-- Find a list of all money spent or earned by the company
+SELECT total_sales AS 'Spent / Earned Money' FROM works_with UNION SELECT salary FROM employee;
+
+-- ----- SQL Join
+-- Find all branches and the names of their managers
+SELECT branch_name AS 'Branch', first_name 'First Name', last_name AS 'Surname'
+FROM employee
+INNER JOIN branch ON emp_id = mgr_id;
+
+SELECT branch_name AS 'Branch', first_name 'First Name', last_name AS 'Surname'
+FROM employee
+LEFT JOIN branch ON emp_id = mgr_id;
+
+SELECT branch_name AS 'Branch', first_name 'First Name', last_name AS 'Surname'
+FROM employee
+RIGHT JOIN branch ON emp_id = mgr_id;
+
+-- ----- Nested Query (more advanced but used a lot, uses IN keyword, not ON like Joins)
+-- Multiple select statements to get specific piece of information
+-- Find names of all employees that have sold over 30000 to a single client
+SELECT employee.first_name, employee.last_name
+FROM employee
+WHERE employee.emp_id IN (
+	-- This executes first, returns a list of emp_ids
+	SELECT works_with.emp_id
+    FROM works_with
+    WHERE works_with.total_sales > 30000
+);
+
+-- Find all clients who are handled by the branch that Michael
+-- Scott manages. Assume you know Michaels' ID (102)
+-- No IN keyword, now using equality "="
+SELECT client_name
+FROM client
+WHERE branch_id = (
+	-- This executes first, returns a branch_id
+	SELECT branch_id
+    FROM branch
+    WHERE mgr_id = 102
+    LIMIT 1  -- just return one
+);
+
+-- ----- On delete (set null and cascade)
+-- DELETE FROM employee WHERE emp_id = 102;
+-- DELETE FROM branch WHERE branch_id = 2;
